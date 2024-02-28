b774ee97875edae2a5ad06f0977ae5ba84bc91b3
David Benito <davben01@ucm.es>
2024-02-28 17:29:09 +0100

    Paso 3 completado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace vivo_con_ello_orgulloso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Vivo con ello orgulloso");
+            }
+          
+        }
+    }
+}
